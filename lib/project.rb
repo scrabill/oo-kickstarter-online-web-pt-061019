c04175c473a49f5c8538233a5f1fe99e92360dd7
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-21 21:50:14 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -16,9 +16,5 @@ class Project
   def add_backer(backer)
     @backers << backer
     backer.backed_projects << self
-    # # binding.pry
-    # backer.back_project(self)
-    #
-    # # self.backers
   end
 end
