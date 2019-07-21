4f49ec2925746d6c0174371c28f5ef186b98c403
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-21 21:29:31 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def backed_projects
+    @backed_projects
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    # add_backer(self)
+    # More Advanced #back_project also adds the backer to the project's backers array
+  end
+end
